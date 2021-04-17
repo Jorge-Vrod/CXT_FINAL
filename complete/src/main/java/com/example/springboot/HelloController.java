8533cde3455dbc1ef8d63e289b6bd432f779c7e9
David Lema Núñez <david.lema.nunez@udc.es>
2021-04-17 23:00:35 +0200

    Creado endpoint /cities{id}

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.springboot;
 
+import com.example.city.City;
+import com.example.city.ReadCityList;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,4 +15,11 @@ public class HelloController {
 		return "Greetings from Spring Boot!";
 	}
 
+	@GetMapping("/cities/{id}")
+	public City cities (@PathVariable Integer id) {
+		ReadCityList cityReader = new ReadCityList();
+
+		cityReader.readFile();
+		return cityReader.getCities().get(id - 1); // Correction factor to ensure that id aligns with positions
+	}
 }
