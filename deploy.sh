5a19def761bd01189852541fc9f736066786424b
David Lema Núñez <david.lema.nunez@udc.es>
2021-05-25 18:01:37 +0200

    Corrección de script

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,15 @@ PORT="22"
 DEPLOY_DIR="/home/git/CXTDir"
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/deploy_rsa # Allow read access to the private key
-ssh-add .travis/deploy_rsa # Add the private key to SSH
-
-echo $?
+chmod 600 deploy_rsa # Allow read access to the private key
+ssh-add deploy_rsa # Add the private key to SSH
 
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
-echo $?
-echo $(git status)
-
 # Skip this command if you don't need to execute any additional commands after deploying.
-#ssh git@$IP -p $PORT <<EOF
-#  cd $DEPLOY_DIR
-#  crystal build --release --no-debug index.cr # Change to whatever commands you need!
-#EOF
+ssh git@$IP -p $PORT <<EOF
+  cd $DEPLOY_DIR
+  crystal build --release --no-debug index.cr # Change to whatever commands you need!
+EOF
