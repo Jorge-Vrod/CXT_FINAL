18233cb50d1b51d70373d363f9d3e91eebe745a7
David Lema Núñez <david.lema.nunez@udc.es>
2021-05-25 18:09:26 +0200

    SSH AUTOMATICO

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ chmod 600 deploy_rsa # Allow read access to the private key
 ssh-add deploy_rsa # Add the private key to SSH
 
 git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR -y -o "StrictHostKeyChecking no"
 git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.
